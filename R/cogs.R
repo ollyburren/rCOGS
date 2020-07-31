798a058866bb44ab311525168f530fe67af37d68
ollyburren <oliver.burren@cimr.cam.ac.uk>
2020-07-31 09:34:10 +0100

    Changes to documentaion to make distinction between PCHiC regions and feature.names - these are subsets of features to include in the analysis

--- R/cogs.R
+++ R/cogs.R
@@ -1,13 +1,13 @@
 #' This function computes COGS scores for a given trait across a set of pchic regions
 #' @param gwas - data.table obtained from load_gwas
-#' @param csnps = GRanges object obtained from load_csnps
+#' @param csnps = GRanges object obtained from make_csnps
 #' @param digest GRanges object obtained from load_digest
-#' @param regions list object obtained from load_pchic
-#' @param feature.set - a character vector of regions to include these must be present in pcHi-C file
+#' @param regions list object obtained from make_pchic
+#' @param feature.names - a character vector of regions to include these must be present in pcHi-C file
 #' @return data.table of results
 #' @export
 
-compute_cogs <- function(gwas,csnps,digest,regions,feature.set){
+compute_cogs <- function(gwas,csnps,digest,regions,feature.names){
   gwas.gr <- with(gwas,GRanges(seqnames=Rle(chr),ranges=IRanges(start=pos,width=1L)))
   ## annotate coding SNPs - we remove these and add back in for gene of interest
   ol.csnps <- findOverlaps(csnps,gwas.gr) %>% as.matrix
@@ -25,16 +25,17 @@ compute_cogs <- function(gwas,csnps,digest,regions,feature.set){
   mDT <- merge(all,ol.DT,by.x='fragid',by.y='fragid')
   mDT[,tissue:=factor(tissue)]
   ## we can compute any score by selecting one or more tissues
-  if(missing(feature.set)){
-      feature.set <- c(names(regions),'coding_snp')
-      message(sprintf("rCOGS:compute_cogs feature.set argument missing computing overall score using %s",paste(feature.set,collapse=",\n",sep=",\n")))
+  if(missing(feature.names)){
+      feature.names <- c(names(regions),'coding_snp')
+      message(sprintf("rCOGS:compute_cogs feature.names argument missing computing overall score using %s",paste(feature.names,collapse=",\n",sep=",\n")))
   }
-  #feature.set <- c(names(regions),'coding_snp')
-  fs.DT <- mDT[tissue %in% feature.set,]
+  #feature.names <- c(names(regions),'coding_snp')
+  fs.DT <- mDT[tissue %in% feature.names,]
   ## remove duplicate fragments by gene so don't count twice
   fs.DT <- fs.DT[!duplicated(fs.DT[,.(fragid,ensg)]),][,.(ensg,ld,frag.ppi,fragid)]
   ## check to see if coding snp is selected if so add
-  if(sum(feature.set=='coding_snp')>0)
+  #if(sum(feature.names=='coding_snp')>0)
+  if(any(feature.names=='coding_snp'))
     fs.DT <- rbind(fs.DT,csnps.DT[,fragid:=0])
   fs.ld.score <- fs.DT[,list(ld.score=sum(frag.ppi)),by=c('ld','ensg')]
   fs.ld.score[,list(cogs=1-prod(1-ld.score)),by='ensg']
